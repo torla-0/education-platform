077d950d1b722482c6e4445fdcda0c689c01d0ab
torla-0 <torlaksuad@gmail.com>
2025-03-26 04:04:14 +0100

    comit message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'learning-resources', component: LearningResourcesComponent },
+  { path: 'quiz', component: QuizComponent },
+  { path: 'blog', component: BlogComponent },
+];
---
+++ src/app/learning-resources/learning-resources.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-resources',
+  standalone: true,
+  imports: [],
+  templateUrl: './learning-resources.component.html',
+  styleUrl: './learning-resources.component.css'
+})
+export class LearningResourcesComponent {
+
+}
